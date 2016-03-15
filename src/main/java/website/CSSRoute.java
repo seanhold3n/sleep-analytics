e87c01b47e1550d4a2e3b929bb3f785fa2f31519
Sean <seanhold3n+github@gmail.com>
2016-03-15 17:55:56 -0400

    CSS file resources now retrieved via getResource(...)

--- src/main/java/website/CSSRoute.java
+++ src/main/java/website/CSSRoute.java
@@ -6,6 +6,7 @@ import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.io.StringWriter;
+import java.net.URISyntaxException;
 
 import spark.Request;
 import spark.Response;
@@ -27,7 +28,7 @@ public class CSSRoute extends Route {
 
 		// Read from the CSS file
 		try (BufferedReader br = new BufferedReader(new FileReader(new File(
-				"src/main/resources/css" + filename)))){
+				getClass().getResource("/css" + filename).toURI())))){
 
 			
 			String line = "";
@@ -40,6 +41,8 @@ public class CSSRoute extends Route {
 			e.printStackTrace();
 		} catch (IOException e) {
 			e.printStackTrace();
+		} catch (URISyntaxException e1) {
+			e1.printStackTrace();
 		}
 
 		response.type("text/css");

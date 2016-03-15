94482ff373f130c83e6e61971204c4979f4ac621
Sean <seanhold3n+github@gmail.com>
2016-03-15 14:50:38 -0400

    Added Date.UTC() thing to SimpleDay, also added more doc.

--- src/main/java/model/SimpleDay.java
+++ src/main/java/model/SimpleDay.java
@@ -2,8 +2,12 @@ package model;
 
 public class SimpleDay { //implements Comparable<SimpleDay> {
 	
+	/** Four-digit year */
 	private final int YEAR;
+	
+	/* One-based month (e.g. 1 = January) */
 	private final int MONTH;
+	
 	/** Day of the month */
 	private final int DAY;
 	
@@ -55,9 +59,22 @@ public class SimpleDay { //implements Comparable<SimpleDay> {
 //		return YEAR*10000 + MONTH*100 + DAY;
 //	};
 
+	/** Returns the date as a string in "MM/DD/YY" format.  For example, March 12th 2016
+	 * would be represented as "03/12/16".
+	 * @see java.lang.Object#toString()
+	 */
 	@Override
 	public String toString(){
-		return String.format("%02d/%02d/%02d", MONTH, DAY, YEAR-2000);
+		return String.format("%02d/%02d/%02d", MONTH, DAY, YEAR-2000); //TODO is -2000 needed w 2 digit truncation?
+	}
+	
+	/** Returns the date as it would be represented by a Date.UTC() call in JavaScript.
+	 * For example, March 12th 2016 would be represented as "Date.UTC(2016,2,12)".
+	 * Note that the month is zero-based.
+	 * @return
+	 */
+	public String toJSDateUTC(){
+		return String.format("Date.UTC(%d,%d,%d)", YEAR, MONTH-1, DAY);
 	}
 
 	

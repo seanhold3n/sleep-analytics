5e254e61c7684d1bfab9f9d4024fd4219c2c298e
Sean Holden <seanhold3n+github@gmail.com>
2016-03-18 21:56:15 -0400

    Added simple moving average (SMA) calculation to DSDM.

--- src/main/java/model/DaySleepDurationMap.java
+++ src/main/java/model/DaySleepDurationMap.java
@@ -1,5 +1,9 @@
 package model;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
 /**
  * @author sean
  */
@@ -35,5 +39,58 @@ public final class DaySleepDurationMap extends DayValuesMap {
 		}
 	}
 
+	
+	
+	/** Get the simple moving averages for each day (that is, the unweighted mean of the previous nDays of data).
+	 * @param nDays The number of days over which to compute the average
+	 * @return A map of all day/SMA pairings
+	 */
+	public DayValuesMap getSimpleMovingAverage(final int nDays){
+		// Create the map
+		DayValuesMap smaMap = new DayValuesMap();
+
+		double sma = 0;
+		boolean firstSMA = true;
+		
+		List<Map.Entry<SimpleDay, Double>> smaActiveSet = new ArrayList<Map.Entry<SimpleDay, Double>>(nDays);
+		
+		for (Map.Entry<SimpleDay, Double> todayEntry : this.entrySet()){
+
+			// If in the first nDays (i.e. not enough items have been added to the set yet), just add them
+			if (smaActiveSet.size() < nDays){
+				smaActiveSet.add(todayEntry);
+			}
+			// Compute SMA
+			else{
+				if (firstSMA){
+					// Use the values in the set
+					for (Map.Entry<SimpleDay, Double> activeEntry : smaActiveSet){
+						sma += activeEntry.getValue();
+					}
+					sma /= nDays;
+					firstSMA = false;
+				}
+				else{
+					// Augment the SMA
+					
+					// Get the nth-back element (i.e. the first one in the list)
+					Map.Entry<SimpleDay, Double> nthElementBack = smaActiveSet.get(0);
+					
+					// Get new SMA based on old one
+					sma = sma + todayEntry.getValue()/nDays - nthElementBack.getValue()/nDays;
+					
+					// Remove the nth-back element and load the new value
+					smaActiveSet.remove(0);
+					smaActiveSet.add(todayEntry);
+					
+				}
+				smaMap.put(todayEntry.getKey(), sma);
+			}
+			
+		}
+		
+		return smaMap;
+		
+	}
 
 }
--- src/test/java/model/DaySleepDurationMapTest.java
+++ src/test/java/model/DaySleepDurationMapTest.java
@@ -13,7 +13,10 @@ import org.junit.Test;
 public class DaySleepDurationMapTest {
 	
 
-	final String TEST_DATA_LOCATION = getClass().getResource("/data/map-test-data.csv").getPath();
+	private final String TEST_DATA_LOCATION = getClass().getResource("/data/map-test-data.csv").getPath();
+	
+	// Max allowed delta for double precision operations
+	private final double DELTA = 0.01;
 	
 	@Test
 	public void testGetInstance() {
@@ -41,52 +44,68 @@ public class DaySleepDurationMapTest {
 		assertEquals(9.0, dsdm.get(sd), 0.05);
 	}
 	
+	@Test
+	public void testGetSimpleMovingAverage() {
+		// Load the map
+		loadMapWithTestData();
+		
+		// Populate the expected results
+		DayValuesMap expectedSMAs = new DayValuesMap();
+		expectedSMAs.put(new SimpleDay(2015, 12, 27), 7.83);
+		expectedSMAs.put(new SimpleDay(2015, 12, 28), 10.27);
+		expectedSMAs.put(new SimpleDay(2015, 12, 29), 8.076);
+		expectedSMAs.put(new SimpleDay(2015, 12, 30), 8.39);
+		expectedSMAs.put(new SimpleDay(2015, 12, 31), 9.07);
+	}
 	
-	/** Performs comprehensive data validation with the test set.  This test includes validation of {@link DaySleepDurationMap#toJSCallback()}.
+	/** Performs comprehensive data validation with the test set.
 	 * @throws IOException
 	 */
 	@Test
-	public void testWithData() throws IOException {
+	public void testWithData() {
+
+		loadMapWithTestData();
+
+		// Make sure the data looks good
+		DaySleepDurationMap dsdm = DaySleepDurationMap.getInstance();
+		assertEquals(7, dsdm.size());
+		assertEquals(4.68, dsdm.get(new SimpleDay(2015, 12, 25)), DELTA);
+		assertEquals(13.08, dsdm.get(new SimpleDay(2015, 12, 26)), DELTA);
+		assertEquals(5.73, dsdm.get(new SimpleDay(2015, 12, 27)), DELTA);
+		assertEquals(12.0, dsdm.get(new SimpleDay(2015, 12, 28)), DELTA);
+		assertEquals(6.50, dsdm.get(new SimpleDay(2015, 12, 29)), DELTA);
+		assertEquals(6.67, dsdm.get(new SimpleDay(2015, 12, 30)), DELTA);
+		assertEquals(14.04, dsdm.get(new SimpleDay(2015, 12, 31)), DELTA);
 		
-		// Max allowed delta for double precision
-		final double DELTA = 0.01;
-		
+	}
+	
+	private void loadMapWithTestData(){
 		// Empty map
 		DaySleepDurationMap.getInstance().clear();
-		
+
 		// Get CSV file
 		try (BufferedReader br = new BufferedReader(new FileReader(new File(TEST_DATA_LOCATION)))){
 			String line = "";
 
 			// Ignore the first line (CSV header)
 			br.readLine();
-			
+
 			while (((line = br.readLine()) != null)){
 				// Convert the line from CSV to a SleepEntry
 				SleepEntry se = SleepEntry.parseFromCSV(line);
-				
+
 //				System.out.println(se.getEffectiveDate());
 //				System.out.println(se.getEffectiveDate().hashCode());
-				
+
 				// Add data from that to the map
 				DaySleepDurationMap.getInstance().addToDay(se.getEffectiveDate(), se.getDuration());
 
 			}
-			
-			// Make sure the data looks good
-			DaySleepDurationMap dsdm = DaySleepDurationMap.getInstance();
-			assertEquals(7, dsdm.size());
-			assertEquals(4.68, dsdm.get(new SimpleDay(2015, 12, 25)), DELTA);
-			assertEquals(13.08, dsdm.get(new SimpleDay(2015, 12, 26)), DELTA);
-			assertEquals(5.73, dsdm.get(new SimpleDay(2015, 12, 27)), DELTA);
-			assertEquals(12.0, dsdm.get(new SimpleDay(2015, 12, 28)), DELTA);
-			assertEquals(6.50, dsdm.get(new SimpleDay(2015, 12, 29)), DELTA);
-			assertEquals(6.67, dsdm.get(new SimpleDay(2015, 12, 30)), DELTA);
-			assertEquals(14.04, dsdm.get(new SimpleDay(2015, 12, 31)), DELTA);
-
-
 		} catch (FileNotFoundException fnfe){
 			fail("File not found, dawg!");
+		} catch (IOException ioe){
+			ioe.printStackTrace();
+			fail("IOException, dawg!");
 		}
 		
 	}

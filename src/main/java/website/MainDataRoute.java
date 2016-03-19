f4295baa74ad8504a63b75254d2e523802cd562c
Sean Holden <seanhold3n+github@gmail.com>
2016-03-18 22:31:58 -0400

    Sleep data is now loaded into DSDM once on server startup.

--- src/main/java/website/MainDataRoute.java
+++ src/main/java/website/MainDataRoute.java
@@ -1,22 +1,11 @@
 package website;
 
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileReader;
-import java.io.IOException;
-
 import model.DaySleepDurationMap;
-import model.SleepEntry;
 import spark.Request;
 import spark.Response;
 import spark.Route;
 
 public class MainDataRoute extends Route {
-
-	public static final String TEST_DATA_LOCATION = RootRoute.class.getClass().getResource(
-			"/data/sean-sleep-data.csv"
-//			"/data/map-test-data.csv"
-			).getPath();
 
 	protected MainDataRoute() {
 		super("/data.json");
@@ -26,41 +15,13 @@ public class MainDataRoute extends Route {
 	public Object handle(Request request, Response response) {
 		// TODO ask user for data from webpage
 		
-		// Clear the map of any old data
-		// TODO move this sort of stuff into the map class and have a populate method or the like (same as FYP project)
-		DaySleepDurationMap.getInstance().clear();
-
-		// Get CSV file
-		try (BufferedReader br = new BufferedReader(new FileReader(new File(TEST_DATA_LOCATION)))){
-			String line = "";
-
-			// Ignore the first line (CSV header)
-			br.readLine();
-			
-			while (((line = br.readLine()) != null)){
-				// Convert the line from CSV to a SleepEntry
-				SleepEntry se = SleepEntry.parseFromCSV(line);
-				
-//				System.out.println(se.getEffectiveDate());
-//				System.out.println(se.getEffectiveDate().hashCode());
-				
-				// Add data from that to the map
-				DaySleepDurationMap.getInstance().addToDay(se.getEffectiveDate(), se.getDuration());
-
-			}
-			
-			br.close();
-
-		} catch (IOException e){
-			e.printStackTrace();
-		}
+		// Note: Sleep data is loaded on server start-up
 		
 		// Prepare the response body
 		String responseStr;
 		
 		// Get the callback parameter
 		String callbackParam = request.queryParams("callback");
-//		System.out.println("Callback: " + callbackParam);
 		// Special thanks to http://stackoverflow.com/a/14621917 for helping me figure out the callback code
 		
 		// Attempt to use callback param
--- src/main/java/website/RunMe.java
+++ src/main/java/website/RunMe.java
@@ -5,14 +5,23 @@ import static spark.Spark.post;
 import static spark.Spark.staticFileLocation;
 import static spark.Spark.setPort;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
 import java.io.IOException;
 
+import model.DaySleepDurationMap;
+import model.SleepEntry;
 import freemarker.template.Configuration;
 
 public class RunMe {
 
 	private final Configuration cfg;
 
+	public static final String DATA_LOCATION = RootRoute.class.getClass().getResource(
+			"/data/sean-sleep-data.csv"
+//			"/data/map-test-data.csv"
+			).getPath();
 
 	public static void main(String[] args) throws IOException {
 		new RunMe();
@@ -20,6 +29,10 @@ public class RunMe {
 
 	public RunMe() throws IOException {
 
+		// Load data
+		loadDataMap();
+		
+		// Start server
 		cfg = createFreemarkerConfiguration();
 		setPort(8081);
 		initializeRoutes();
@@ -44,7 +57,34 @@ public class RunMe {
 		// data - /data.json
 		get(new MainDataRoute());
 
+	}
 	
+	private void loadDataMap() throws IOException {
+		// Clear the map of any old data
+		// TODO move this sort of stuff into the map class and have a populate method or the like (same as FYP project)
+		DaySleepDurationMap.getInstance().clear();
+
+		// Get CSV file
+		BufferedReader br = new BufferedReader(new FileReader(new File(DATA_LOCATION)));
+		String line = "";
+
+		// Ignore the first line (CSV header)
+		br.readLine();
+
+		while (((line = br.readLine()) != null)){
+			// Convert the line from CSV to a SleepEntry
+			SleepEntry se = SleepEntry.parseFromCSV(line);
+
+//			System.out.println(se.getEffectiveDate());
+//			System.out.println(se.getEffectiveDate().hashCode());
+
+			// Add data from that to the map
+			DaySleepDurationMap.getInstance().addToDay(se.getEffectiveDate(), se.getDuration());
+
+		}
+
+		br.close();
+		
 	}
 	
 }
